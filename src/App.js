7e34fc3e0368ca1b39f6e2aebd558ba2e07814cb
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-11-21 09:34:09 +0300

    created child div for the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        ibrahim nexchat app
+      <div className="blur"></div>
+      <div className="blur"></div>
     </div>
   );
 }
