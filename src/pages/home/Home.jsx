d9739c801d0e32609b6371820fe7c58bbfa00a94
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-11-21 10:04:26 +0300

    added sections of the chat app

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='profileSide'>Profile</div>
+        <div className='postSide'>Post</div>
+        <div className='RightSide'>Rightside</div>
+
+    </div>
+  )
+}
+
+export default Home
