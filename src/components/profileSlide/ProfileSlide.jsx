96c27c49ac068eb3c67dac364098970fa292bd62
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-11-23 00:04:09 +0300

    imported profilecard to the profile slide

--- src/components/profileSlide/ProfileSlide.jsx
+++ src/components/profileSlide/ProfileSlide.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard.jsx/ProfileCard'
 
 const ProfileSlide = () => {
   return (
     <div className='ProfileSlide'>
         <LogoSearch/>
+        <ProfileCard/>
     </div>
   )
 }
