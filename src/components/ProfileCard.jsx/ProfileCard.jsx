52dc81fdfd96963db815c51504f748ff64ae4997
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-11-23 21:22:56 +0300

    added profile image inside profile card

--- src/components/ProfileCard.jsx/ProfileCard.jsx
+++ src/components/ProfileCard.jsx/ProfileCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.jpg'
 
 const ProfileCard = () => {
   return (
-    <div>ProfileCard</div>
+    <div className="ProfileCard">
+      <div className="ProfileImages">
+        <img src={Profile} alt="" />
+      </div>
+    </div>
   )
 }
 
