724731b818527f469ada5c4acf299be77db467dc
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-11-23 21:29:51 +0300

    profile editing

--- src/components/ProfileCard.jsx/ProfileCard.jsx
+++ src/components/ProfileCard.jsx/ProfileCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Cover from '../../img/cover.jpg'
 import Profile from '../../img/profileImg.jpg'
+import './ProfileCard.css'
 
 const ProfileCard = () => {
   return (
     <div className="ProfileCard">
       <div className="ProfileImages">
+        <img src={Cover} alt="" />
         <img src={Profile} alt="" />
       </div>
     </div>
