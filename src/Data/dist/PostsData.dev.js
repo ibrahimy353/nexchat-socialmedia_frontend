865bed21467b2ff6ba82d688e739f58423a1f092
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-12-06 20:40:27 +0300

    post edit

---
+++ src/Data/dist/PostsData.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PostsData = void 0;
+
+var _postpic = _interopRequireDefault(require("../img/postpic1.jpg"));
+
+var _postpic2 = _interopRequireDefault(require("../img/postpic2.jpg"));
+
+var _postpic3 = _interopRequireDefault(require("../img/postpic3.JPG"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var PostsData = [{
+  img: _postpic["default"],
+  name: 'Tzuyu',
+  desc: "Happy New Year all friends! #2023",
+  likes: 2300,
+  liked: true
+}, {
+  img: _postpic2["default"],
+  name: 'Maryam',
+  desc: "Party time :)",
+  likes: 2300,
+  liked: false
+}, {
+  img: _postpic3["default"],
+  name: "Salena Gomez",
+  desc: "At Archery Festival",
+  likes: 800,
+  liked: false
+}];
+exports.PostsData = PostsData;
