6c4f84f20b7129f9f92a844302e8f06f002d62a9
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-12-19 22:25:00 +0300

    imported profile card and profileside inside the profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import ProfileCard from '../../components/ProfileCard.jsx/ProfileCard'
 import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
+import PostSide from '../../PostSide/PostSide'
 import './Profile.css'
 const Profile = () => {
   return (
     <div className="Profile">
         <ProfileLeft/>
+
+        <div className="Profile-center">
+            <ProfileCard/>
+            <PostSide/>
+        </div>
     </div>
 
   )
