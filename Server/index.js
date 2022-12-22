0f86ba8066b3d5a8c9167474c689f64ed286464b
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-12-22 20:09:41 +0300

    aditing index

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,10 @@ import mongoose, { mongo } from 'mongoose';
 
 const app = express ();(
 app.use (bodyParser.json({limit:'30mb', extended: true}))
-app.use (bodyParser.urlencoded({limit:'30mb', extended: true})) 
+app.use (bodyParser.urlencoded({limit:'30mb', extended: true}));
 
-mongoose.connect("mongodb+srv://Ibrahim:<Dresser3141>@cluster0.6nvwbtp.mongodb.net/SocialMedia_Youtube?retryWrites=true&w=majority")
+mongoose
+.connect(
+    "mongodb+srv://Ibrahim:<Dresser3141>@cluster0.6nvwbtp.mongodb.net/SocialMedia_Youtube?retryWrites=true&w=majority"
+    )
+    .then(()=>app.listen(5000, ()=> console.log("Listening"));
