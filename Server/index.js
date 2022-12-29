d6a1ad23666d9cfa25350bdd174645b57961ccf3
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2022-12-29 14:26:41 +0300

    updating the index.js

--- Server/index.js
+++ Server/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose, { mongo } from 'mongoose';
 
-const app = express ();(
-app.use (bodyParser.json({limit:'30mb', extended: true}))
+const app = express ();
+app.use (bodyParser.json({limit:'30mb', extended: true}));
 app.use (bodyParser.urlencoded({limit:'30mb', extended: true}));
 
 mongoose
