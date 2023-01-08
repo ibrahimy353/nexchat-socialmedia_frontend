2a03d074229377f61fb702078911e1691e24d6bd
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-09 01:52:08 +0300

    added url

--- Server/server.js
+++ Server/server.js
@@ -8,5 +8,7 @@ app.use (bodyParser.urlencoded({limit:'30mb', extended: true}));
 
 mongoose
 .connect(
-    "mongodb+srv://Ibrahim:12ibrahimyusuf@cluster0.6nvwbtp.mongodb.net/SocialMedia_Youtube?retryWrites=true&w=majority")
+    "mongodb+srv://Ibrahim:12ibrahimyusuf@cluster0.6nvwbtp.mongodb.net/SocialMedia_Youtube?retryWrites=true&w=majority",
+    {useNewUrlParser: true, useUnifiedTopology: true})
+
     .then(() => app.listen(5000, ()=> console.log("Listening")));
