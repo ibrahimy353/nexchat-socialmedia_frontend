7a5b802c7be258e85b7d943efcc4fa916e1a459a
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-10 17:37:35 +0300

    editing the server.js

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,16 @@
 
-'use strict';
-import express from 'express';
+import  express  from 'express';
 import bodyParser from 'body-parser';
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
+const app= express();
 
-const app = express ();
+app.use (bodyParser.json)
 app.use (bodyParser.json({limit:'30mb', extended: true}));
 app.use (bodyParser.urlencoded({limit:'30mb', extended: true}));
 
-mongoose
-.connect(
-    "mongodb+srv://project1:12ibrahimyusuf@cluster0.xdzxmtz.mongodb.net/project 0?retryWrites=true&w=majority",
-    {useNewUrlParser: true, useUnifiedTopology: true})
 
-    .then(() => app.listen(5000, ()=> console.log("Listening")));
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
+
+
