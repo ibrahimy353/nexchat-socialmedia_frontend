00e6a341c5924d69703220d769b1a8ca5ec59f8a
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-11 16:53:18 +0300

    server edit

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,32 @@ app.use (bodyParser.json)
 app.use (bodyParser.json({limit:'30mb', extended: true}));
 app.use (bodyParser.urlencoded({limit:'30mb', extended: true}));
 
+//database
+const database = ( module.exports = () =>{
+   
+    const connectionParams ={
+ 
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+    }
+    
+    try {
+         mongoose.connect("mongodb+srv://hussein:12ibrahimyusuf@cluster0.7ahpa1g.mongodb.net/?retryWrites=true&w=majority",
+         
+         connectionParams
+         );
+ 
+         console.log("Database connection successfully");
+ 
+    } catch (error) {
+         console.log("error");
+         console.log("Database connection failed");
+ 
+    }
+ });
+  database();
+ 
+
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
